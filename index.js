b3f62a0717eee9cdca7a08063ba0c6c68caef7a0
Gathsara2002 <liviru2002mmg@gmail.com>
2024-01-04 17:31:46 +0530

    Check require and its functionalities

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const myName="Gathsara";
-console.log(myName);
+const os = require("os");
+const path = require("path");
+
+console.log(os.platform()); //win32
+console.log(os.version()); //Windows 10 Home Single Language
+
+console.log(__dirname); //E:\RAD\Learning-NodeJS\Node-tuto
+console.log(__filename); //E:\RAD\Learning-NodeJS\Node-tuto\index.js
+
+console.log(path.basename(__filename)); //index.js
+console.log(path.extname(__filename)); //.js
