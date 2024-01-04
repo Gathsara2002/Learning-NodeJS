0e0308a8b8d0c408339e3f53dad885cc6bc27ec1
Gathsara2002 <liviru2002mmg@gmail.com>
2024-01-04 17:41:21 +0530

    import vs require

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const os = require("os");
-const path = require("path");
+//const os = require("os");
+//const path = require("path");
+
+import os from 'os';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 console.log(os.platform()); //win32
 console.log(os.version()); //Windows 10 Home Single Language
@@ -9,3 +16,12 @@ console.log(__filename); //E:\RAD\Learning-NodeJS\Node-tuto\index.js
 
 console.log(path.basename(__filename)); //index.js
 console.log(path.extname(__filename)); //.js
+console.log(path.parse(__filename)); 
+
+// {
+//     root: 'E:\\',
+//     dir: 'E:\\RAD\\Learning-NodeJS\\Node-tuto',
+//     base: 'index.js',
+//     ext: '.js',
+//     name: 'index'
+//   }
