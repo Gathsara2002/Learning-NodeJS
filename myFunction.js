7e2978fa1d13349da376d76632723faf99b62f30
Gathsara2002 <liviru2002mmg@gmail.com>
2024-01-04 21:00:05 +0530

    Checked export and import in node

---
+++ myFunction.js
@@ -0,0 +1,13 @@
+const math = (num1, num2, num3, num4) => {
+  let a = num1 + num2 * 2;
+  if (num3) {
+    a += num3 - 5;
+  }
+
+  if (num4) {
+    a = a * num4;
+  }
+  return a;
+};
+
+export default math;
