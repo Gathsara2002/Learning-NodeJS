48e2ca24a12c90c4c0f2a133959a5f7f9e996e26
Gathsara2002 <liviru2002mmg@gmail.com>
2024-01-18 10:43:40 +0530

    create cardGen

---
+++ libs/htmlCardGen.js
@@ -0,0 +1,20 @@
+const cardGent = (name, age, stuClass, subjects = []) => {
+
+  let sub = "";
+  subjects?.forEach((ele) => {
+    sub += `<p>${ele}</p>`;
+  });
+
+  return `<div class="card">
+    <h1 class="name">${name}</h1>
+    <div class="infor">
+      <p>age is : ${age}</p>
+      <p>class is : ${stuClass}</p>
+    </div>
+    <h3>Subjects</h3>
+    <div class="subjects">
+      ${sub}
+    </div>
+  </div>`;
+  
+};
