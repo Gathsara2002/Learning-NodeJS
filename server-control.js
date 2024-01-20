ec7cb2471139ef5583b0eb6cf08c88e057bcc9d9
Gathsara2002 <liviru2002mmg@gmail.com>
2024-01-20 18:40:52 +0530

    Server finished with data transfer

--- server-control.js
+++ server-control.js
@@ -1,8 +1,16 @@
 import { log } from "node:console";
+import { fileRead } from "./old/libs/read-file.js";
+import { selectContentType } from "./old/libs/select-content-type.js";
 
 export const server = (req, res) => {
   if (req.url === "/" && req.method === "GET") {
-    res.writeHead(200, "Content-Type:text/html");
-    res.end('<h1>Hello</h1>');
+    fileRead("public/index.html", res, "text/html");
+  } else if (req.method === "GET") {
+    const extention = String(req.url).split(".");
+    fileRead(
+      `public${req.url}`,
+      res,
+      selectContentType(extention[extention.length - 1])
+    );
   }
 };
